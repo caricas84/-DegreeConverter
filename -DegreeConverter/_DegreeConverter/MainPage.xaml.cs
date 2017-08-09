566e0aef72302f348974ceca4e1f96b8c4c6fef4
CAROLINA CASTA�O GONZALEZ <carolinacastano90799@correo.itm.edu.co>
2017-08-08 21:39:48 -0500

    Degree Converter

---
+++ -DegreeConverter/_DegreeConverter/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace _DegreeConverter
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            ConvertButton.Clicked += ConvertButton_Clicked;
+        }
+            private void ConvertButton_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(DegreeEntry.Text))
+            {
+                DisplayAlert("Error", "You must enter a value in degree.", "Accept");
+                return;
+            }
+
+            var degree = decimal.Parse(DegreeEntry.Text);
+
+            var fahrenheit = (degree * 1.8M) + 32;
+            
+            FahrenheitEntry.Text = string.Format("{0:N2}", fahrenheit);
+        }
+    }
+}
