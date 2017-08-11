5b0dc8826bd2935eab5055014015d1261caf33f4
CAROLINA CASTA�O GONZALEZ <carolinacastano90799@correo.itm.edu.co>
2017-08-10 21:37:22 -0500

    Updates App

--- -DegreeConverter/_DegreeConverter/MainPage.xaml.cs
+++ -DegreeConverter/_DegreeConverter/MainPage.xaml.cs
@@ -13,6 +13,7 @@ namespace _DegreeConverter
         {
             InitializeComponent();
             ConvertButton.Clicked += ConvertButton_Clicked;
+            OtherButton.Clicked += OtherButton_Clicked;
         }
             private void ConvertButton_Clicked(object sender, EventArgs e)
         {
@@ -25,8 +26,26 @@ namespace _DegreeConverter
             var degree = decimal.Parse(DegreeEntry.Text);
 
             var fahrenheit = (degree * 1.8M) + 32;
-            
+                     
+
             FahrenheitEntry.Text = string.Format("{0:N2}", fahrenheit);
+            
+        }
+
+        private void OtherButton_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(DegreeEntry.Text))
+            {
+                DisplayAlert("Error", "You must enter a value in degree.", "Accept");
+                return;
+            }
+
+            var degree = decimal.Parse(DegreeEntry.Text);
+
+            var centigrade = (degree - 32) / 1.8M;
+            
+            CentigradeEntry.Text = string.Format("{0:N2}", centigrade);
+           
         }
     }
 }
